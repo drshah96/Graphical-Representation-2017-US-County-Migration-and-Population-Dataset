50614be827ba0a012d11dfd4e4115b14678cea91
drshah96 <drshah96@gmail.com>
2020-01-12 15:00:56 -0500

    Introduction

---
+++ Q7.R
@@ -0,0 +1,15 @@
+########### Q7 ##########
+
+library("readxl")
+library("pacman")
+library("dplyr")
+library("ggplot2")
+install.packages("ggpubr")
+library("ggpubr")
+library(datasets)
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(pacman, party, rio, tidyverse)
+
+
+summary(state.abb)
