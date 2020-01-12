71984eade50f26669ad1c82f4b8e883d6d041ad4
drshah96 <drshah96@gmail.com>
2020-01-12 10:50:45 -0500

    Graph Pending

--- Q2.R
+++ Q2.R
@@ -3,60 +3,48 @@
 library("readxl")
 library("pacman")
 library("dplyr")
+library("ggplot2")
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(pacman, party, rio, tidyverse)
 
 States_MigrationRate_raw <- import("Data/Module 1 Project Data.xlsx") %>%
     as.tibble() %>%
     select("State","International Migration Rate")
-
 States_MigrationRate_raw <- as.data.frame(States_MigrationRate_raw)
 
-States_MigrationRate_s <- table(States_MigrationRate_raw$State)
-States_MigrationRate_s <- as.data.frame(States_MigrationRate_s)
-States_MigrationRate_s
+#States_MigrationRate_m
+#States_MigrationRate_s <- table(States_MigrationRate_raw$State)
+#States_MigrationRate_s <- as.data.frame(States_MigrationRate_s)
+#States_MigrationRate_s
 
-States_MigrationRate_m <- table(States_MigrationRate_raw$`International Migration Rate`)
-States_MigrationRate_m <- as.data.frame(States_MigrationRate_m)
-names(States_MigrationRate_m) <- c("International Migration Rate","Frequency")
-class(States_MigrationRate_m)
+#States_MigrationRate_m <- table(States_MigrationRate_raw$`International Migration Rate`)
+#States_MigrationRate_m <- as.data.frame(States_MigrationRate_m)
+#names(States_MigrationRate_m) <- c("International Migration Rate","Frequency")
 
-m1 <- merge(States_MigrationRate_m, States_MigrationRate_raw, by = c(States_MigrationRate_m$`International Migration Rate`,States_MigrationRate_raw$`International Migration Rate`))
+#m <- data.frame(States_MigrationRate_m$`International Migration Rate`, States_MigrationRate_raw$`International Migration Rate`)
+#m1 <- merge(States_MigrationRate_m, States_MigrationRate_raw, by = c(States_MigrationRate_m$`International Migration Rate`,States_MigrationRate_raw$`International Migration Rate`))
+#m2 <- group_by(States_MigrationRate_raw, States_MigrationRate_raw$`International Migration Rate`)
+#m2
 
-States_MigrationRate_raw
-States_MigrationRate <- as.data.frame(States_MigrationRate_raw)
+States_MigrationRate <- States_MigrationRate_raw %>%
+    group_by(State) %>%
+    summarize(m3 = sum(`International Migration Rate`))
+names(States_MigrationRate) <- c("States","Frequency")
+States_MigrationRate
+
+hist(States_MigrationRate$Frequency, freq = T, ylim = c(0,300), xlim, breaks = States_MigrationRate$Frequency)
 
 suppressPackageStartupMessages(library(dplyr))
-sum(States_MigrationRate$`International Migration Rate`)
-d <- States_MigrationRate %>%
+#sum(States_MigrationRate$`International Migration Rate`)
+
+States_MigrationRate <- States_MigrationRate %>%
     mutate(
-        cumsum = cumsum(States_MigrationRate$`International Migration Rate`),
-        freq = round(States_MigrationRate$`International Migration Rate` / sum(States_MigrationRate$`International Migration Rate`), 3),
+        cumsum = cumsum(States_MigrationRate$Frequency),
+        freq = round(States_MigrationRate$Frequency / sum(States_MigrationRate$Frequency), 3),
         cum_freq = cumsum(freq)
     )
-d
-tail(d$cum_freq)
-sum(d$freq)
-hist(d$freq)
+States_MigrationRate
+tail(States_MigrationRate$States)
+
+hist(States_MigrationRate$States)
 hist(d$cum_freq)
-
-
-library(ggplot2)
-# Basic histogram
-ggplot(d, aes(x=d$freq)) + geom_histogram()
-# Change colors
-p<-ggplot(d, aes(x=d$freq)) + 
-    geom_histogram(color="black", fill="white")
-p
-
-ggplot(d, aes(x=d$freq, color=d$State)) +
-    geom_histogram(fill="white")
-# Overlaid histograms
-ggplot(d, aes(x=d$freq, color=d$State)) +
-    geom_histogram( alpha=0.5, position="identity")
-
-ggplot(d, aes(x=d$freq, fill=d$State, color=d$State)) +
-    geom_histogram(position="identity")
-
-p<-ggplot(d, aes(x=d$freq, fill=d$State, color=d$State)) +
-    geom_histogram(position="identity", alpha=0.5)
