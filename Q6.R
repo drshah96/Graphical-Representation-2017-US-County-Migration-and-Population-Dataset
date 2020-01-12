23ab0373181593e59a8c394fdd7e661163f8ee81
drshah96 <drshah96@gmail.com>
2020-01-12 15:00:40 -0500

    Graph

--- Q6.R
+++ Q6.R
@@ -4,7 +4,7 @@ library("readxl")
 library("pacman")
 library("dplyr")
 library("ggplot2")
-install.packages("ggpubr")
+#install.packages("ggpubr")
 library("ggpubr")
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(pacman, party, rio, tidyverse)
@@ -23,11 +23,21 @@ Domestic_MigrationRate_raw
 
 International_MigrationRate <- International_MigrationRate_raw %>%
     group_by(State) %>%
-    summarize(Frequency = sum(`International Migration Rate`))
+    summarize(Intl_Frequency = sum(`International Migration Rate`))
 International_MigrationRate
 
 Domestic_MigrationRate <- Domestic_MigrationRate_raw %>%
     group_by(State) %>%
-    summarize(Frequency = sum(`Domestic Migration Rate`))
+    summarize(Dom_Frequency = sum(`Domestic Migration Rate`))
 Domestic_MigrationRate
 
+MigrationRate <- as.data.frame(International_MigrationRate)
+MigrationRate$Dom_Frequency <- Domestic_MigrationRate$Dom_Frequency
+MigrationRate
+
+ggplot(MigrationRate, aes(x=MigrationRate$Intl_Frequency, y=MigrationRate$Dom_Frequency)) + 
+    geom_point() + 
+    geom_text(label=MigrationRate$State, 
+              hjust=1.5, vjust = 0,
+              check_overlap = TRUE) + 
+    geom_smooth(method=lm)
