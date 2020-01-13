5b54a1ef92aa454dc6ff9c4d42f0fbcdcf5a492a
drshah96 <drshah96@gmail.com>
2020-01-13 15:31:42 -0500

    Q1 a - Complete

--- Q1.R
+++ Q1.R
@@ -30,33 +30,6 @@ d
 
 def_par <- par() 
 
-## New margins
-par(mar=c(5,5,4,5)) 
-
-## bar plot, pc will hold x values for bars
-pc = barplot(d$`2017 Population`,  
-             width = 1, space = 0.2, border = NA, axes = F,
-             ylim = c(0, 1.05 * max(d$cumsum, na.rm = T)), 
-             ylab = "Cummulative Counts" , cex.names = 0.7, 
-             names.arg = d$State,
-             main = "Pareto Chart (version 1)")
-
-## Cumulative counts line 
-lines(pc, d$cumsum, type = "b", cex = 0.7, pch = 19, col="cyan4")
-
-## Framing plot
-box(col = "grey62")
-
-## adding axes
-axis(side = 2, at = c(0, d$cumsum), las = 1, col.axis = "grey62", col = "grey62", cex.axis = 0.8)
-axis(side = 4, at = c(0, d$cumsum), labels = paste(c(0, round(d$cum_freq * 100)) ,"%",sep=""), 
-     las = 1, col.axis = "cyan4", col = "cyan4", cex.axis = 0.8)
-
-## restoring default paramenter
-par(def_par) 
-
-def_par <- par() 
-
 # New margins
 par(mar=c(5,5,4,5)) 
 
@@ -64,12 +37,12 @@ par(mar=c(5,5,4,5))
 pc = barplot(d$`2017 Population`,
              width = 1, space = 0.2, border = NA, axes = F,
              ylim = c(0, 1.05 * max(d$`2017 Population`, na.rm = T)), 
-             ylab = "Counts" , cex.names = 0.7, 
+             ylab = "Population" , xlab = "States", cex.names = 0.7, 
              names.arg = d$State,
-             main = "Pareto Chart (version 2)")
+             main = "Top 20 States - Pareto Chart")
 
 ## anotate left axis
-axis(side = 2, at = c(0, d$`2017 Population`), las = 1, col.axis = "grey62", col = "grey62", tick = T, cex.axis = 0.8)
+axis(side = 2, at = c(0, round(d$`2017 Population`,2)), las = 1, col.axis = "grey62", col = "grey62", tick = T, cex.axis = 0.8)
 
 ## frame plot
 box( col = "grey62")
@@ -97,9 +70,9 @@ States_Population_2017_raw <- as.data.frame(States_Population_2017_raw)
 States_Population_2017 <- States_Population_2017_raw[order(-States_Population_2017_raw$`2017 Population`),]
 States_Population_2017
 
-States_Population_2017 <- States_Population_2017 %>%
-    group_by(State) %>%
-    summarize(m3 = max(`2017 Population`))
+#States_Population_2017$m3 <- States_Population_2017 %>%
+#    group_by(State) %>%
+#     max(`2017 Population`)
 
 States_Population_2017 <- States_Population_2017[1:20,]
 States_Population_2017
